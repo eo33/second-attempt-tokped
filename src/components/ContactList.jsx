7672a20e6413b0cee7fdcc77610ce8e56b2ca797
lasto-ed12 <70526829+eo33@users.noreply.github.com>
2023-09-15 23:21:20 +0700

    minor adjustment

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -42,33 +42,6 @@ function ContactList() {
     },[data])
 
     // Update isFavorite to keep in sync with contactList
-    /*
-    useEffect(()=>{
-        
-        let favCount = Object.keys(isFavorite).length
-        let contactCount = contactList.length
-        let newID = {}
-        if(contactCount > favCount){
-            newID = {...isFavorite};
-            for(let contact of contactList){
-                // If contact id not in newID, add it
-                if(!(contact.id in newID)){
-                    newID[contact.id] = false
-                }
-            }
-        } else {
-            // Create a brand new fav list
-            contactList.forEach(contact=>newID[contact.id] = false)
-            // Update the new fav list based on previous favorite
-            for(let id in isFavorite){
-                if(id in newID){
-                    newID[id] = isFavorite[id]
-                }
-            }
-        }
-        setIsFavorite(newID)
-    }, [contactList])
-    */
     useEffect(() => {
         // Calculate the new isFavorite object without modifying the existing one
         let newIsFavorite = { ...isFavorite };
@@ -93,159 +66,161 @@ function ContactList() {
     <>
         {addMode ? <AddContact contactList={contactList} addMode={addMode} refetch={refetch} handleAddMode={handleAddMode} /> :
             <div>
-                <div className='col-12 contact-list'>
-                <div className="row mt-1">
-                    <h2 className="col display-4 d-flex align-items-center mt-2">
-                    {contactMode}
-                    </h2>                
-                    <div className="col d-flex justify-content-end align-items-center mx-0 px-0 mt-3">
-                        {contactMode === "Contacts" ? 
-                            <>
-                                <button 
-                                    className='d-flex flex-column align-items-center button-style px-2' 
-                                    onClick={()=>{
-                                        setContactMode('Favorite')
-                                        setShowFavorite(true)
-                                    }}>
-                                    <i class="fa-solid fa-star fa-2x"></i>
-                                    <span>Fav</span>
-                                </button>
+                <div className='col-12 col-md-6 contact-list'>
+                    {/**CONTACTS NAVBAR */}
+                    <div className="row mt-1">
+                        <h2 className="col display-4 d-flex align-items-center mt-2">
+                            {contactMode}
+                        </h2>                
+                        <div className="col d-flex justify-content-end align-items-center mx-0 px-0 mt-3">
+                            {contactMode === "Contacts" ? 
+                                <>
+                                    <button 
+                                        className='d-flex flex-column align-items-center button-style px-2' 
+                                        onClick={()=>{
+                                            setContactMode('Favorite')
+                                            setShowFavorite(true)
+                                        }}>
+                                        <i class="fa-solid fa-star fa-2x"></i>
+                                        <span>Fav</span>
+                                    </button>
 
-                                <button 
-                                    className='d-flex flex-column align-items-center button-style px-2'
-                                    onClick={()=>{setAddMode(true)}}
-                                >
-                                    <i className="fa-solid fa-plus fa-2x"></i>
-                                    <span>Add</span>
-                                </button>
+                                    <button 
+                                        className='d-flex flex-column align-items-center button-style px-2'
+                                        onClick={()=>{setAddMode(true)}}
+                                    >
+                                        <i className="fa-solid fa-plus fa-2x"></i>
+                                        <span>Add</span>
+                                    </button>
 
-                                <button 
-                                    className='d-flex flex-column align-items-center button-style px-2' 
-                                    onClick={()=>setContactMode('Delete')}
-                                >
-                                    <i className="fa-solid fa-trash fa-2x"></i>
-                                    <span>Delete</span>
-                                </button> 
-                            </>: 
-                            <>
-                                <button 
-                                    className='d-flex flex-column align-items-center button-style px-2'
-                                    onClick={()=>{
-                                        setContactMode('Contacts')
-                                        setShowFavorite(false)
-                                    }}
-                                >
-                                    <i class="fa-solid fa-arrow-rotate-left fa-2x"></i>
-                                    <span>Back</span>
-                                </button>
-                            </>
-                        }
+                                    <button 
+                                        className='d-flex flex-column align-items-center button-style px-2' 
+                                        onClick={()=>setContactMode('Delete')}
+                                    >
+                                        <i className="fa-solid fa-trash fa-2x"></i>
+                                        <span>Delete</span>
+                                    </button> 
+                                </>: 
+                                <>
+                                    <button 
+                                        className='d-flex flex-column align-items-center button-style px-2'
+                                        onClick={()=>{
+                                            setContactMode('Contacts')
+                                            setShowFavorite(false)
+                                        }}
+                                    >
+                                        <i class="fa-solid fa-arrow-rotate-left fa-2x"></i>
+                                        <span>Back</span>
+                                    </button>
+                                </>
+                            }
+                        </div>
                     </div>
-                </div>
-                <div className="row mt-3">
-                    <div className="col search-bar d-flex align-items-center gap-2">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                    <input 
-                        type="text" 
-                        placeholder="Search" 
-                        aria-label="Search"
-                        onChange={event=>{
-                            event.preventDefault();
-                            setSearchQuery(event.target.value)
-                            setPage(1)
-                        }}
-                    />
-                    </div>      
-                </div>
-                </div>
-                {
-                    // Filter when it is in favorite, or when it is in delete mode. Also, only print 10 contacts
-                    //.filter(contact => (isFavorite[contact.id] === showFavorite) || (contactMode==="Delete"))
-                    contactList
-                        .filter(contact => (isFavorite[contact.id] === showFavorite) || (contactMode==="Delete"))
-                        .slice((page-1)*10,page*10)
-                        .map(contact=>(
-                        <>
-                            <div className="row contact-list mt-4">
-                                <div className="col-2 col-md-1">
-                                {/*Profile picture*/}
-                                    <i class="fa-solid fa-user profile-pic p-2 fa-2x"></i>
-                                </div>
-                                <div className="col mx-lg-2">
-                                    <div className="row full-name">
-                                        {/*First name + last name*/}
-                                        <div className="col">
-                                        {contact.first_name+' '+contact.last_name}
+                    {/**SEARCHBAR */}
+                    <div className="row mt-2 py-2">
+                        <div className="col search-bar d-flex align-items-center gap-2">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <input 
+                            type="text" 
+                            placeholder="Search" 
+                            aria-label="Search"
+                            onChange={event=>{
+                                event.preventDefault();
+                                setSearchQuery(event.target.value)
+                                setPage(1)
+                            }}
+                        />
+                        </div>      
+                    </div>
+                    {/**CONTACTLIST */}
+                    {
+                        // Logic: filter when it is in favorite, or when it is in delete mode. Also, only print 10 contacts
+                        contactList
+                            .filter(contact => (isFavorite[contact.id] === showFavorite) || (contactMode==="Delete"))
+                            .slice((page-1)*10,page*10)
+                            .map(contact=>(
+                            <>
+                                <div className="row contact-list mt-2 border-top pt-2">
+                                    <div className="col-2 col-lg-1">
+                                    {/*Profile picture*/}
+                                        <i class="fa-solid fa-user profile-pic p-2 fa-2x"></i>
+                                    </div>
+                                    <div className="col mx-lg-2">
+                                        <div className="row full-name">
+                                            {/*First name + last name*/}
+                                            <div className="col">
+                                            {contact.first_name+' '+contact.last_name}
+                                            </div>
+                                        </div>
+                                        <div className="row phone-number">
+                                            {/*Phone number*/}
+                                            <div className="col">
+                                            {contact.phones[0] !== undefined ? contact.phones[0].number : null}
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className="row phone-number">
-                                        {/*Phone number*/}
-                                        <div className="col">
-                                        {contact.phones[0] !== undefined ? contact.phones[0].number : null}
-                                        </div>
-                                    </div>
-                                </div>
 
-                                <div className="col favorite d-flex justify-content-end align-items-center">
-                                {/*When user click on button set isFav to true for that ID. Only show when it is not in delete mode*/}
-                                { contactMode !== "Delete" ? 
-                                    <>
-                                        <button 
-                                            className='button-style' 
-                                            type='button'
-                                            onClick={()=>{
-                                                setIsFavorite( (prevState) => ({
-                                                    ...prevState,
-                                                    [contact.id]: !prevState[contact.id]
-                                                }))
-                                            }}
-                                        >
-                                            <i className={`button-style fa-star fa-2x ${isFavorite[contact.id] ? "fa-solid" : "fa-regular"}`}></i>
-                                        </button>
-                                    </>:null
-                                }
-                                {
-                                    contactMode === 'Delete' ? 
-                                        <button 
-                                            className='button-style' 
-                                            type='button'
-                                            onClick={()=>{
-                                                // Delete from Apollo
-                                                deleteUser({
-                                                    variables: {
-                                                        "id": contact.id
-                                                    },
-                                                    refetchQueries: [GET_CONTACT_LIST],
-                                                    awaitRefetchQueries:true,
-                                                })
-                                            }}
-                                        >
-                                            <i className="button-style fa-solid fa-trash fa-2x"></i>
-                                        </button> : null
+                                    <div className="col favorite d-flex justify-content-end align-items-center">
+                                    {/*When user click on button set isFav to true for that ID. Only show when it is not in delete mode*/}
+                                    { contactMode !== "Delete" ? 
+                                        <>
+                                            <button 
+                                                className='button-style' 
+                                                type='button'
+                                                onClick={()=>{
+                                                    setIsFavorite( (prevState) => ({
+                                                        ...prevState,
+                                                        [contact.id]: !prevState[contact.id]
+                                                    }))
+                                                }}
+                                            >
+                                                <i className={`button-style fa-star fa-2x ${isFavorite[contact.id] ? "fa-solid" : "fa-regular"}`}></i>
+                                            </button>
+                                        </>:null
                                     }
-                                </div>
-                            </div> 
-                        </>
-                    ))
-                }
-                {/**Pagination*/}
-                <div className="row d-flex justify-content-center align-items-center button-style mt-4">
-                    <div className="col-auto">
-                    <button 
-                        className={`button-style ${page === 1 ? "button-disabled": ""}`}
-                        onClick={()=>{setPage(e=>e-1)}}
-                        >
-                        <i className="fa-solid fa-chevron-left mx-0 px-0"></i>
-                    </button>
-                    </div>
-                    <div className="col-auto mx-0 px-0">{page}</div>
-                    <div className="col-auto">
-                    <button 
-                        className={`button-style ${page === Math.ceil(availableContact/10) ? "button-disabled": ""}`}
-                        onClick={()=>{setPage(e=>e+1)}}
-                        >
-                        <i className="fa-solid fa-chevron-right mx-0 px-0"></i>
-                    </button>
+                                    {
+                                        contactMode === 'Delete' ? 
+                                            <button 
+                                                className='button-style' 
+                                                type='button'
+                                                onClick={()=>{
+                                                    // Delete from Apollo
+                                                    deleteUser({
+                                                        variables: {
+                                                            "id": contact.id
+                                                        },
+                                                        refetchQueries: [GET_CONTACT_LIST],
+                                                        awaitRefetchQueries:true,
+                                                    })
+                                                }}
+                                            >
+                                                <i className="button-style fa-solid fa-trash fa-2x"></i>
+                                            </button> : null
+                                        }
+                                    </div>
+                                </div> 
+                            </>
+                        ))
+                    }
+                    {/**Pagination*/}
+                    <div className="row d-flex justify-content-center align-items-center button-style mt-4">
+                        <div className="col-auto">
+                        <button 
+                            className={`button-style ${page === 1 ? "button-disabled": ""}`}
+                            onClick={()=>{setPage(e=>e-1)}}
+                            >
+                            <i className="fa-solid fa-chevron-left mx-0 px-0"></i>
+                        </button>
+                        </div>
+                        <div className="col-auto mx-0 px-0">{page}</div>
+                        <div className="col-auto">
+                        <button 
+                            className={`button-style ${page === Math.ceil(availableContact/10) ? "button-disabled": ""}`}
+                            onClick={()=>{setPage(e=>e+1)}}
+                            >
+                            <i className="fa-solid fa-chevron-right mx-0 px-0"></i>
+                        </button>
+                        </div>
                     </div>
                 </div>
             </div>
